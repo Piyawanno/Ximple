91626a62fa63726753d29d676bdfdf58f951970c
Piyawanno <k.piyawanno@gmail.com>
2011-05-24 22:13:01 +0700

    refact ModelColumn

---
+++ include/javascript/emailcolumn.js
@@ -0,0 +1,38 @@
+$(document).ready(
+	$('.email_field').keyup(function(){
+		var reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(!reg.test($(this).val())){
+			$('#info_'+$(this).attr('id')).css({color:'red'});
+			$('#info_'+$(this).attr('id')).html(imgWrong + emailNotOk);
+			formLock = true;
+		}else{
+			var exist = false;
+			if($(this).attr('id') == 'user_email'){
+				var uri;
+				if(insertUser){
+					uri = sectionURI+'userinfo_check_email_exists/email/';
+				}else{
+					uri = sectionURI+'userinfo_check_email_exists/uid/'+uid+'/email/';
+				}
+				if(isInstalled){
+					$.get(uri+$(this).val(), function(data){
+						if(data != 'not exist'){
+							exist = true;
+							formLock = true;
+							$('#info_user_email').css({color:'red'});
+							$('#info_user_email').html(imgWrong + emailExists);
+						}
+					});
+				}else{
+					exist = false;
+				}
+			}
+			if(!exist){
+				$('#info_'+$(this).attr('id')).html(imgCorrect);
+				formLock = false;
+			}else{
+				formLock = true;
+			}
+		}
+	});
+);
