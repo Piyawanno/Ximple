4217b7311bd1f983cb21235ff4b6883a5fa80ba3
Kittipong Piyawanno <k.piyawanno@gmail.com>
2011-05-25 11:21:03 +0700

    refactor ModelColumn

---
+++ include/javascript/textcolumn.js
@@ -0,0 +1,22 @@
+$(document).ready(	
+	$(".text").cleditor({
+		height		:	'400px',
+		width		:	'100%',
+		controls	:	"bold italic underline strikethrough subscript superscript | font size " +
+						"style | color highlight removeformat | bullets numbering | outdent " +
+						"indent quote | alignleft center alignright justify | undo redo | "+
+						"rule advancedimage icon table link unlink | cut copy paste pastetext | print code source"
+	});
+	
+	$(".shorttext").cleditor({
+		height		:	'250px',
+		width		:	'100%',
+		controls	:	"bold image italic underline strikethrough bullets numbering quote link unlink icon paste pastetext source"
+	});
+	
+	$(".usershorttext").cleditor({
+		height		:	'250px',
+		width		:	'100%',
+		controls	:	"bold advancedimage italic underline strikethrough bullets numbering quote link unlink icon paste pastetext code source"
+	});
+);
