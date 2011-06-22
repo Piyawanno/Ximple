d6dda1e3b9de3ff06595ea29b9b3051c1ab75f65
Kittipong Piyawanno <k.piyawanno@gmail.com>
2011-06-22 23:13:08 +0700

    develop DevelCard

--- include/javascript/color.column.js
+++ include/javascript/color.column.js
@@ -1,18 +1,17 @@
 $(document).bind("ready", function(){
 	$('.color_picker').each(function(){
-		if(!$(this).attr('initialized')){
-			formID = this.attr('id');
-			$("#picker_"+formID).farbtastic("#"+formID);
-			$("#picker_"+formID).dialog({
-				bgiframe : true,
-				autoOpen : false,
-				modal : true,
-				width : 220,
-				height : 250,
-				title : selectColorLabel
-			});
-			$("#"+formID).click(function(){$("#picker_"+formID).dialog("open");});
-		}
-		$(this).attr('initialized', true);
+		formID = $(this).attr('id');
+		$("#picker_"+formID).farbtastic("#"+formID);
+		$("#picker_"+formID).dialog({
+			bgiframe : true,
+			autoOpen : false,
+			modal : true,
+			width : 220,
+			height : 250,
+			title : selectColorLabel
+		});
+		$("#"+formID).click(function(){
+			$("#picker_"+$(this).attr('id')).dialog("open");
+		});
 	});
 });
