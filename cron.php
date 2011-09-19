af3de067d3359c06ea154d89d74ea6922fceabb1
Kittipong Piyawanno <k.piyawanno@gmail.com>
2011-09-19 20:38:01 +0700

    Cron-Job & DB-Backup

--- cron.php
+++ cron.php
@@ -1,6 +1,11 @@
 <?php
-	define('SECTION', 1);
-	define('SECTION_PATH', dirname(__FILE__).'/');
-	require('include/tool/boot.tool.inc');
-	boot_cron();
+	if(PHP_SAPI === 'cli'){
+		define('SECTION', 1);
+		define('SECTION_PATH', dirname(__FILE__).'/');
+		define('XIMPLE_CRON_PROCESS', 'Cron Process');
+		require('include/tool/boot.tool.inc');
+		boot_cron();
+	}else{
+		die('Cron Job can only be started from CLI.');
+	}
 ?>
