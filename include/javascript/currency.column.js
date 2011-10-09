f3259acf7b29ef6794af09bea165008b7caa80e8
Kittipong Piyawanno <k.piyawanno@gmail.com>
2011-10-10 01:17:12 +0700

    currency column

--- include/javascript/currency.column.js
+++ include/javascript/currency.column.js
@@ -1,6 +1,23 @@
+$(document).bind("ready", function(){
+	initForm($(".currency_main"), function(form){
+		form.spinbox({
+			min  : 0,
+			max  : 4294967295,
+			step : 1
+		});
+	});
+	
+	initForm($(".currency_sub"), function(form){
+		form.spinbox({
+			min  : 0,
+			max  : 99,
+			step : 1
+		});
+	});
+});
+
 $(document).bind("submit", function(){
 	var realCurrency = $('currency');
 	var mainCurrency = $('currency_main');
-	var subCurrency = $('currency_sub');
-	for(i=0;i<;)
+	var subCurrency  = $('currency_sub');
 });
